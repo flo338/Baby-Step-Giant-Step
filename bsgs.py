3b2da8f580601d528e8ce71b085c7810900c320e
flo338 <84997136+flo338@users.noreply.github.com>
2022-02-13 02:12:25 +0100

    Update bsgs.py

--- bsgs.py
+++ bsgs.py
@@ -7,12 +7,12 @@ def bsgs(p: int, g: int, x: int):
 	"""
 	m = math.floor(math.sqrt(p))
 
-		g_j = {pow(g,j,p): j for j in range(1, m+1)}
+	g_j = {pow(g,j,p): j for j in range(1, m+1)}
 
-		for i in range(1, m+1):
-	    		y = pow(g, -i*m, p) * x % p
-	    		if g_j.get(y):
-	        		return i*m+g_j.get(y)
+	for i in range(1, m+1):
+    		y = pow(g, -i*m, p) * x % p
+    		if g_j.get(y):
+        		return i*m+g_j.get(y)
 
 
 if __name__ == '__main__':
@@ -20,15 +20,4 @@ if __name__ == '__main__':
 	g = 2135
 	x = 5999
 
-	import math
-
-	m = math.floor(math.sqrt(p))
-
-	g_j = {pow(g,j,p): j for j in range(1, m+1)}
-
-	for i in range(1, m+1):
-    		y = pow(g, -i*m, p) * x % p
-    		if g_j.get(y):
-        		print(i*m+g_j.get(y))
-        		break
-    
+	print(bsgs(p, g, x))
